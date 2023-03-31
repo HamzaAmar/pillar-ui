0f2eff55ff16766b24b1d7c74e8a7aee3c01f4ba
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-03-31 02:30:48 +0000

    fix: typo problem in icons #21

--- packages/pillar-icons/src/icons/action/cloudDownload.tsx
+++ packages/pillar-icons/src/icons/action/cloudDownload.tsx
@@ -1,10 +1,10 @@
 import { svgProps } from '../../shared'
 import type { SvgType } from '../../type'
 
-const bellX = (props: SvgType) => (
+const cloudDownload = (props: SvgType) => (
   <svg {...svgProps} {...props}>
     <path d="M19 18a3.5 3.5 0 0 0 0-7h-1a4.6 4.6 0 0 0-.8-3.8A6 6 0 0 0 11.4 5a6 6 0 0 0-2 .8A5 5 0 0 0 7 9c-1 0-2.1.3-3 .9-.8.6-1.4 1.5-1.7 2.5-.2 1 0 2 .4 3 .4.8 1.2 1.6 2.2 2M12 13v9m0 0-3-3m3 3 3-3" />
   </svg>
 )
 
-export default bellX
+export default cloudDownload
--- packages/pillar-icons/src/icons/transportation/tank.tsx
+++ packages/pillar-icons/src/icons/transportation/tank.tsx
@@ -3,7 +3,7 @@ import type { SvgType } from '../../type'
 
 const tank = (props: SvgType) => (
   <svg {...svgProps} {...props}>
-    <path d="M7 15v0m10 2a2 2 0 1 0 4 0 2 2 0 0 0-4 0Zm0 0h-6.5m9.5-1.8V11a1 1 0 0 0-1-1h-6l-2-5H5v6.5M18 5h-1a1 1 0 0 0-1 1v4m-5 5a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z" />
+    <path d="M4 15 15 4m5 5L9 20m-4-8 7 7M8.5 8.5l7 7M12 5l7 7" />
   </svg>
 )
 
