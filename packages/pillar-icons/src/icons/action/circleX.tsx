9c4ec943fa81b3639a80845717bd7626ff89afb1
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-03-31 15:55:16 +0000

    fix: name problem from multiply to x in  actions icons #39 #21

--- packages/pillar-icons/src/icons/action/circleX.tsx
+++ packages/pillar-icons/src/icons/action/circleX.tsx
@@ -1,10 +1,10 @@
 import { svgProps } from '../../shared'
 import type { SvgType } from '../../type'
 
-const circleMultiply = (props: SvgType) => (
+const circleX = (props: SvgType) => (
   <svg {...svgProps} {...props}>
     <path d="m10 10 4 4m0-4-4 4m11-2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
   </svg>
 )
 
-export default circleMultiply
+export default circleX
