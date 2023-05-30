1d996a08c394bbef045bbb2d58195c0389c4347b
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-05-30 16:08:55 +0100

    feat: Add useBooleanState custom hook and corresponding tests
    This commit introduces the useBooleanState custom hook, which provides
    convenient functionality for managing a boolean state. The hook
    includes functions for setting the boolean value to true, false,
    or toggling it. It also accepts an optional default value.
    
    The commit includes comprehensive test cases using the renderHook
    function from @testing-library/react. The tests cover various scenarios,
    such as checking the initial boolean value, initializing with a default
    value, setting the value to true and false, and toggling the value.
    
    By adding this custom hook and its associated tests, we enhance code
    reusability and ensure the reliability of boolean state management.

---
+++ packages/pillar-hooks/src/useBoolean/useBoolean.test.tsx
@@ -0,0 +1,48 @@
+import { renderHook, act } from '@testing-library/react'
+import { useBooleanState } from './useBoolean'
+import { test, describe, expect } from 'vitest'
+
+describe('useBooleanState', () => {
+  test('should return an object with the current boolean value and functions for updating it', () => {
+    const { result } = renderHook(() => useBooleanState())
+
+    expect(result.current.booleanValue).toBe(false)
+    expect(result.current.setBooleanValue).toBeDefined()
+    expect(result.current.setTrue).toBeDefined()
+    expect(result.current.setFalse).toBeDefined()
+    expect(result.current.setToggle).toBeDefined()
+  })
+  test('should initialize with the provided default value', () => {
+    const { result } = renderHook(() => useBooleanState(true))
+
+    expect(result.current.booleanValue).toBe(true)
+  })
+
+  test('should set the boolean value to true when the setTrue function is called', () => {
+    const { result } = renderHook(() => useBooleanState())
+
+    act(() => result.current.setTrue())
+
+    expect(result.current.booleanValue).toBe(true)
+  })
+
+  test('should set the boolean value to false when the setFalse function is called', () => {
+    const { result } = renderHook(() => useBooleanState())
+
+    act(() => result.current.setFalse())
+
+    expect(result.current.booleanValue).toBe(false)
+  })
+
+  test('should toggle the boolean value when the setToggle function is called', () => {
+    const { result } = renderHook(() => useBooleanState())
+
+    act(() => result.current.setToggle())
+
+    expect(result.current.booleanValue).toBe(true)
+
+    act(() => result.current.setToggle())
+
+    expect(result.current.booleanValue).toBe(false)
+  })
+})
--- packages/pillar-hooks/src/useBoolean/useBoolean.tsx
+++ packages/pillar-hooks/src/useBoolean/useBoolean.tsx
@@ -1,15 +1,29 @@
 import { useCallback, useState } from 'react'
 
 /**
- * Custom hook for managing a boolean state value.
- *
- * @param {boolean} [defaultValue] - The initial value of the boolean state default to false.
- * @returns {Object} An object containing the current boolean value and functions for updating it.
+ * Custom hook for managing a boolean state with convenient update functions.
+ * @param {boolean} [defaultValue=false] - The initial value for the boolean state.
+ * @returns {{
+ *   booleanValue: boolean,
+ *   setBooleanValue: React.Dispatch<React.SetStateAction<boolean>>,
+ *   setTrue: () => void,
+ *   setFalse: () => void,
+ *   setToggle: () => void
+ * }}
  */
 export function useBooleanState(defaultValue: boolean = false) {
   const [booleanValue, setBooleanValue] = useState(!!defaultValue)
+  /**
+   * Sets the boolean value to `true`.
+   */
   const setTrue = useCallback(() => setBooleanValue(true), [])
+  /**
+   * Sets the boolean value to `false`.
+   */
   const setFalse = useCallback(() => setBooleanValue(false), [])
+  /**
+   * Toggles the boolean value.
+   */
   const setToggle = useCallback(() => setBooleanValue((value) => !value), [])
 
   return { booleanValue, setBooleanValue, setTrue, setFalse, setToggle }
