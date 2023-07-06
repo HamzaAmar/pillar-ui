96acad95e0ccfa252ece6e4e1cd464e0044dfaa5
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-07-06 20:20:56 +0100

    refactor: code components
    - import classnames from pillar utils
    - add default values of the component (css properties)
    - change the name of the component to be more readable

--- packages/pillar-core/src/core/code/code.tsx
+++ packages/pillar-core/src/core/code/code.tsx
@@ -1,13 +1,13 @@
 import { forwardRef } from 'react'
 import { CodeProps } from './code.type'
 import type { ForwardRefComponent } from '../../types/polymorphic.type'
-import { classnames } from '../../utils'
+import { classnames } from '@pillar/utils'
 
 const Code = forwardRef((props, ref) => {
-  const { as: Tag = 'code', color = 'slate', size = 'md', corner = 'sm', children, className, ...rest } = props
-  const _className = classnames(`code l_size-${size} u_${color} l_corner-${corner}`, { [className!]: !!className })
+  const { as: Tag = 'code', color = 'surface', size = 'md', corner = 'sm', children, className, ...rest } = props
+  const classNames = classnames(`code u_size-${size} u_${color} u_corner-${corner}`, { [className!]: !!className })
   return (
-    <Tag ref={ref} className={_className} {...rest}>
+    <Tag ref={ref} className={classNames} {...rest}>
       {children}
     </Tag>
   )
