fd3f62f16fe16540930f802370a4f81eb2e60c04
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-07-06 19:18:29 +0100

    refactor: clean up index.ts and change the tsup build to work correct with project need

--- packages/pillar-icons/tsup.config.ts
+++ packages/pillar-icons/tsup.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  // entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/icons/**/*.tsx'],
   format: ['cjs', 'esm'],
   treeshake: true,
   splitting: false,
