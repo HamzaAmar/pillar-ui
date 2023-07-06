9c29302920114a1ad98735d302646504dd1f00a6
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-07-06 20:46:40 +0100

    refactor: paper components
    - add padding prop to the paper
    - add margin support as well
    - add border support also
    - import classnames from utils

--- packages/pillar-core/src/core/paper/paper.tsx
+++ packages/pillar-core/src/core/paper/paper.tsx
@@ -2,46 +2,97 @@ import { forwardRef } from 'react'
 import type { CSSProperties } from 'react'
 import { ForwardRefComponent } from '../../types/polymorphic.type'
 import type { PaperProps } from './paper.type'
-import { classnames } from '../../utils'
+import { classnames } from '@pillar/utils'
+
+const direction = ['Top', 'Right', 'Bottom', 'Left']
 
 // Helper function to generate CSS variable
 function getCSSVariable(value: string | undefined, initial: number = 9) {
   if (!value) return
   const [color, degree = initial] = value.split('-')
-  return `var(--${color}-${degree})`
+  return `var(--${color}-${degree}, none)`
+}
+
+function generatedProperties(arr: (string | undefined)[], prefix: string) {
+  let result = {}
+  const [shortProperty, ...rest] = arr
+  if (!!shortProperty) {
+    result = { [prefix]: `var(--space-${shortProperty})` }
+  }
+  rest.forEach((val, index) => {
+    if (!!val) {
+      const property = `${prefix}${direction[index]}`
+      result = { ...result, [property]: `var(--space-${val})` }
+    }
+  })
+
+  return result
 }
 
 const paper = forwardRef((props, ref) => {
-  const {
-    background = 'slate-1',
+  let {
+    background,
     color,
-    padding,
+    p,
+    plr,
+    ptb,
+    pb = ptb,
+    pt = ptb,
+    pr = plr,
+    pl = plr,
+    m,
+    mlr,
+    mtb,
+    mb = mtb,
+    mt = mtb,
+    mr = mlr,
+    ml = mlr,
+    b,
+    blr,
+    btb,
+    bb = btb,
+    bt = btb,
+    br = blr,
+    bl = blr,
     shadow,
     width,
     height,
-    corner = 'sharp',
+    corner,
     as: Tag = 'div',
-    spacing,
+    borderColor,
+    borderWidth,
+    borderStyle,
+    borderPosition,
+    flow,
     children,
     className,
+    style = {},
     ...rest
   } = props
+  const paddingProperties = generatedProperties([p, pt, pr, pb, pl], 'padding')
+  const marginProperties = generatedProperties([m, mt, mr, mb, ml], 'margin')
+  const borderProperties = generatedProperties([b, bt, br, bb, bl], 'border')
 
-  const _className = classnames(`hello l_corner-${corner}`, {
+  const classNames = classnames(`paper`, {
     [`u_shadow-${shadow}`]: !!shadow,
-    [`l_flow__${spacing}`]: !!spacing,
+    [`l_flow__${flow}`]: !!flow,
+    [`u_corner-${corner}`]: !!corner,
     [className!]: !!className,
   })
+
   const _style: CSSProperties = {
     background: getCSSVariable(background),
     color: getCSSVariable(color, 12),
-    padding: padding ? `var(--space-${padding})` : undefined,
-    width: width ? `${width}` : undefined,
-    height: height ? `${height}` : undefined,
+    width: `${width}`,
+    height: `${height}`,
+    ...paddingProperties,
+    ...marginProperties,
+    ...borderProperties,
+    ...style,
   }
 
   return (
-    <Tag style={_style} className={_className} ref={ref} {...rest}>
+    <Tag style={_style} className={classNames} ref={ref} {...rest}>
       {children}
     </Tag>
   )
--- packages/pillar-core/src/core/paper/paper.type.ts
+++ packages/pillar-core/src/core/paper/paper.type.ts
@@ -5,16 +5,50 @@ import type { Color, Corner, Size } from '../../types'
 type Step = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12'
 type ColorShade = Color | `${Color}-${Step}`
 type Shadow = Corner
-export interface PaperProps {
+
+export interface Margin {
+  m?: string
+  mt?: string
+  mr?: string
+  mb?: string
+  ml?: string
+  mtb?: string
+  mlr?: string
+}
+
+export interface Padding {
+  p?: string
+  pt?: string
+  pr?: string
+  pb?: string
+  pl?: string
+  ptb?: string
+  plr?: string
+}
+
+export interface Border {
+  b?: string
+  bt?: string
+  br?: string
+  bb?: string
+  bl?: string
+  btb?: string
+  blr?: string
+}
+export interface PaperProps extends Padding, Margin, Border {
   background?: ColorShade
-  // color : 'red' | 'danger' | 'success' | 'green' | 'orange' | 'warning' | 'indigo'
+  // color : 'danger' | 'danger' | 'success' | 'success' | 'orange' | 'warning' | 'primary'
   color?: ColorShade
   // corner types 'sharp' | 'xs' | 'sm'  | 'md' | 'lg' | 'xl'| 'circle' | 'full'
   corner?: Corner
   // size types '2xs' | 'xs' | 'sm'  | 'md' | 'lg' | 'xl'| '2xl'
   padding?: Size
-  spacing?: Omit<Size, '2xl' | '2xs'>
+  flow?: Size
   shadow?: Shadow
   width?: CSSProperties['width']
   height?: CSSProperties['height']
+  borderPosition?: 'left' | 'right' | 'top' | 'bottom' | 'block' | 'inline' | 'all'
+  borderStyle?: CSSProperties['borderStyle']
+  borderColor?: ColorShade
+  borderWidth?: CSSProperties['borderStyle']
 }
