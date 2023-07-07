5ad30e18fdf40219a9976169876873b04eddf3bf
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-07-07 13:40:35 +0100

    refactor: spinner component
    - add default value
    - import classnames from utils

--- packages/pillar-core/src/core/spinner/spinner.tsx
+++ packages/pillar-core/src/core/spinner/spinner.tsx
@@ -1,11 +1,12 @@
 import { CSSProperties } from 'react'
-import { classnames } from '../../utils/classnames'
+import { classnames } from '@pillar/utils'
 import { SpinnerProps } from './spinner.type'
 
-const spinner = ({ color = 'primary', size = 'sm', thickness = '2px', invert = false, ...rest }: SpinnerProps) => {
-  const className = classnames(`spinner l_size-${size} u_${color}`, {
-    [`u_${color}`]: Boolean(color),
+const spinner = ({ color = 'primary', size, thickness = '2px', invert = false, ...rest }: SpinnerProps) => {
+  const className = classnames(`spinner u_${color}`, {
+    [`u_${color}`]: !!color,
     ['spinner__invert']: invert,
+    [`u_size-${size}`]: !!size,
   })
   return <div style={{ '--border': thickness } as CSSProperties} className={className} {...rest} />
 }
