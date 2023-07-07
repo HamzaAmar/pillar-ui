6d8d4a14969cf9520b8b2ffe488d26fe73d79bad
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-07-07 19:20:26 +0100

    refactor: correct the name of wallmart icon

--- packages/pillar-icons/src/icons/brand/walmart.tsx
+++ packages/pillar-icons/src/icons/brand/walmart.tsx
@@ -1,10 +1,10 @@
 import { svgProps } from '../../shared'
 import type { SvgType } from '../../type'
 
-const SvgComponent = (props: SvgType) => (
+const walmart = (props: SvgType) => (
   <svg {...svgProps} {...props}>
     <path d="M12 8V3m3.5 7L20 7.5M15.5 14l4.5 2.5m-8-.5v5m-3.5-7L4 16.5M8.5 10 4 7.5" />
   </svg>
 )
 
-export default SvgComponent
+export default walmart
