4cde33121f2b72a86013765735c84aa245caae84
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-07-07 17:44:52 +0100

    feat: add app and document file

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
-import { useTheme } from '@pillar/core'
 import '@pillar/core/main.min.css'
 import '../scss/main.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  useTheme()
   return <Component {...pageProps} />
 }
---
+++ apps/docs/src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="custom-scroll">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
