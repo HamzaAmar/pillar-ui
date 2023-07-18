574f48cb497847bcc20e5f2f8a371854c30ac32c
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-07-18 12:26:49 +0100

    fix:  contentlayer mdx error and fix typescript error

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
-import '@pillar/core/main.min.css'
+import '@pillar/core/style'
 import '../scss/main.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
