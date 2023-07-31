758e5c1364eb4881c8e188dc36f8f3172994038a
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-07-31 17:33:51 +0100

    feat: add useTimeout custom hook

---
+++ packages/pillar-hooks/src/useTimeout/useTimeout.test.ts
@@ -0,0 +1,76 @@
+import { renderHook, act } from '@testing-library/react'
+import { useTimeout } from './useTimeout'
+import { it, describe, expect, vi } from 'vitest'
+
+describe('useTimeout custom hook', () => {
+  vi.useFakeTimers()
+
+  it('test valid callback and delay', () => {
+    const callback = vi.fn()
+    renderHook(() => useTimeout(callback, 100))
+    expect(callback).not.toHaveBeenCalled()
+    vi.advanceTimersByTime(100)
+    expect(callback).toHaveBeenCalled()
+  })
+
+  it('test callback executed after delay', () => {
+    const callback = vi.fn()
+    renderHook(() => useTimeout(callback, 100))
+    expect(callback).not.toHaveBeenCalled()
+    vi.advanceTimersByTime(100)
+    expect(callback).toHaveBeenCalled()
+  })
+
+  it('test clear timer function cancels timer', () => {
+    const callback = vi.fn()
+    const { result } = renderHook(() => useTimeout(callback, 100))
+    expect(callback).not.toHaveBeenCalled()
+    act(() => {
+      result.current()
+    })
+    vi.advanceTimersByTime(100)
+    expect(callback).not.toHaveBeenCalled()
+  })
+
+  // it('test invalid callback type', () => {
+  //   expect(() => {
+  //     // @ts-ignore
+  //     renderHook(() => useTimeout('not a function', 100))
+  //   }).toThrow('useTimeout: expects its parameter to be a function, but it received a [ string')
+  // })
+
+  // it('test delay is NaN', () => {
+  //   expect(() => {
+  //     renderHook(() => useTimeout(() => {}, NaN))
+  //   }).toThrow('useTimeout: delay parameter cannot be NaN.')
+  // })
+
+  it('test callback not executed if component unmounts', () => {
+    const callback = vi.fn()
+    const { unmount } = renderHook(() => useTimeout(callback, 100))
+    expect(callback).not.toHaveBeenCalled()
+    unmount()
+    vi.advanceTimersByTime(100)
+    expect(callback).not.toHaveBeenCalled()
+  })
+
+  it('test delay can be set to 0', () => {
+    const callback = vi.fn()
+    renderHook(() => useTimeout(callback, 0))
+    vi.advanceTimersByTime(0)
+    expect(callback).toHaveBeenCalled()
+  })
+
+  it('test clear timer function can be called multiple times', () => {
+    const callback = vi.fn()
+    const { result } = renderHook(() => useTimeout(callback, 100))
+    expect(callback).not.toHaveBeenCalled()
+    act(() => {
+      result.current()
+      result.current()
+      result.current()
+    })
+    vi.advanceTimersByTime(100)
+    expect(callback).not.toHaveBeenCalled()
+  })
+})
---
+++ packages/pillar-hooks/src/useTimeout/useTimeout.ts
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useCallback } from 'react'
+import { usePersistentCallback } from '../usePersistentCallback'
+
+type TimeoutHandler = () => void
+
+const validateParameters = (callback: TimeoutHandler | undefined, delay: number) => {
+  if (typeof callback !== 'function') {
+    throw new Error(`useTimeout: expects its parameter to be a function, but it received a [ ${typeof callback}`)
+  }
+  if (delay < 0) {
+    throw new Error('useTimeout: The delay should not be smaller than 0.')
+  }
+
+  if (isNaN(delay)) {
+    throw new Error('useTimeout: delay parameter cannot be NaN.')
+  }
+}
+
+export const useTimeout = (callback: TimeoutHandler, delay: number = 150) => {
+  const savedCallback = useRef<TimeoutHandler>()
+  const timerRef = useRef<NodeJS.Timer | null>(null)
+  const persistedCallback = usePersistentCallback(callback)
+
+  validateParameters(persistedCallback, delay)
+
+  const clearTimer = useCallback(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current)
+    }
+  }, [])
+
+  useEffect(() => {
+    savedCallback.current = persistedCallback
+  }, [persistedCallback])
+
+  useEffect(() => {
+    timerRef.current = setTimeout(() => {
+      savedCallback.current?.()
+    }, delay)
+
+    return clearTimer
+  }, [delay, clearTimer])
+
+  return clearTimer
+}
+
+export default useTimeout
