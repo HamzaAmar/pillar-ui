f174408bc1614ec28e5e5e944f9c915b62dcbf0a
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-07-31 17:28:17 +0100

    feat: add useInterval custom hook

---
+++ packages/pillar-hooks/src/useInterval/useInterval.test.ts
@@ -0,0 +1,47 @@
+import { renderHook, act } from '@testing-library/react'
+import { useInterval } from './useInterval'
+import { test, describe, expect, vi } from 'vitest'
+
+describe('useInterval custom hook', () => {
+  test('test valid parameters', () => {
+    const mockFn = vi.fn()
+    const { result } = renderHook(() => useInterval(mockFn, 100))
+
+    expect(result.current.start).toBeDefined()
+    expect(result.current.stop).toBeDefined()
+    expect(result.current.toggle).toBeDefined()
+    expect(typeof result.current.start).toBe('function')
+    expect(typeof result.current.stop).toBe('function')
+    expect(typeof result.current.toggle).toBe('function')
+  })
+
+  test('test default value', () => {
+    const callback = vi.fn()
+    const { result } = renderHook(() => useInterval(callback, 100))
+    expect(result.current.active).toBe(false)
+  })
+
+  test('test default value', () => {
+    const callback = vi.fn()
+    const { result } = renderHook(() => useInterval(callback, 100))
+    expect(result.current.active).toBe(false)
+    act(() => {
+      result.current.start()
+    })
+    expect(result.current.active).toBe(true)
+  })
+
+  test('test default value', () => {
+    const callback = vi.fn()
+    const { result } = renderHook(() => useInterval(callback, 100))
+    expect(result.current.active).toBe(false)
+    act(() => {
+      result.current.toggle()
+    })
+    act(() => {
+      result.current.toggle()
+    })
+
+    expect(result.current.active).toBe(false)
+  })
+})
