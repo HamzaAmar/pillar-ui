892f15a1896f12f1e8d7907837eca43a02e1c99c
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-10-22 16:33:07 +0100

    feat: add seo meta to the project

--- apps/docs/src/component/common/layout/layout.tsx
+++ apps/docs/src/component/common/layout/layout.tsx
@@ -1,11 +1,43 @@
 import React, { useState } from 'react'
+import Head from 'next/head'
 import { Footer, Header, MenuBar, TableOfContent } from '..'
 import { Flex, Paper } from '@pillar-ui/core'
-import type { DocsLayoutProps, LayoutProps } from './layout.type'
+import type { DocsLayoutProps, LayoutProps, MetaProps } from './layout.type'
+import { DOMAIN } from 'src/constant/domain'
+import { useRouter } from 'next/router'
 
-export const Layout = ({ children }: LayoutProps) => {
+export const Layout = ({ children, title, image = '/darkHero.png', description, ...rest }: LayoutProps) => {
+  const router = useRouter()
+
+  const meta: MetaProps = {
+    title: `${title} – Pillar UI library`,
+    description,
+    image: image,
+    ...rest,
+  }
+
   return (
     <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:url" content={`${DOMAIN}${router.asPath}`} />
+        <link rel="canonical" href={`${DOMAIN}${router.asPath}`} />
+
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Hamza Miloud Amar" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@pillar_ui" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+
+        {meta.date && <meta property="article:published_time" content={meta.date} />}
+      </Head>
       <Header />
       <main className="l_flex-1">{children}</main>
       <Footer />
@@ -13,16 +45,47 @@ export const Layout = ({ children }: LayoutProps) => {
   )
 }
 
-export const DocsLayout = ({ children, headings }: DocsLayoutProps) => {
+export const DocsLayout = ({ children, headings, title, image, ...rest }: DocsLayoutProps) => {
   const [open, setOpen] = useState(false)
 
   function handleToggle() {
     console.log('Hello this is the value of open', open)
     setOpen((open) => !open)
   }
+  const router = useRouter()
+
+  const imageIfExist = image ? { image: `https://miloudamar.com/${image}` } : {}
+
+  const meta: MetaProps = {
+    title: `${title} – Pillar UI library`,
+    description: rest.excerpt,
+    ...imageIfExist,
+    ...rest,
+  }
 
   return (
     <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta property="og:url" content={`${DOMAIN}${router.asPath}`} />
+        <link rel="canonical" href={`${DOMAIN}${router.asPath}`} />
+
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Hamza Miloud Amar" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@pillar_ui" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+
+        {meta.date && <meta property="article:published_time" content={meta.date} />}
+      </Head>
+
       <Header handleToggle={handleToggle} />
       <Flex>
         <MenuBar open={open} />
--- apps/docs/src/component/common/layout/layout.type.ts
+++ apps/docs/src/component/common/layout/layout.type.ts
@@ -1,11 +1,20 @@
 import type { ReactNode } from 'react'
 import type { Components } from 'contentlayer/generated'
 
-export interface LayoutProps {
+export interface MetaProps {
+  title?: string
+  description?: string
+  image?: string
+  type?: string
+  date?: string
+}
+
+export interface LayoutProps extends MetaProps {
   children: ReactNode
 }
 
-export interface DocsLayoutProps {
+export interface DocsLayoutProps extends MetaProps {
   children: ReactNode
   headings?: Components['headings']
+  excerpt: string
 }
