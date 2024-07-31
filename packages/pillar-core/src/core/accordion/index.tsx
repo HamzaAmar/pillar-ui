18796714a5085e1110ee0eb04337b41018f7cafb
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2024-07-31 19:00:35 +0100

    refactor: accordion by rename CSS class, update     component for performance and uniqueness.

--- packages/pillar-core/src/core/accordion/index.tsx
+++ packages/pillar-core/src/core/accordion/index.tsx
@@ -15,6 +15,49 @@ import type {
 
 /*
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+  Accordion  Section
+///////////////////////////////////////////////////////////////////////////////////////////////////
+*/
+
+export const Accordion = forwardRef((props, forwardedRef) => {
+  const {
+    children,
+    type = 'single',
+    collapsible,
+    color = 'bg',
+    variant,
+    size = 'md',
+    corner = 'sharp',
+    separate,
+    className,
+    ...rest
+  } = props
+  const state = useAccordion(props)
+
+  const classNames = classnames(`a-c`, {
+    [`u_${color}`]: !!color,
+    [`u_f-${size}`]: !!size,
+    l_flow__sm: !!separate,
+    [className!]: !!className,
+  })
+  const accordionContext = { ...state, variant, corner }
+
+  return (
+    <div ref={forwardedRef} className={classNames} {...rest}>
+      <AccordionProvider {...accordionContext}>
+        {Children.map(children, (child, index) => {
+          if (!isValidElement(child)) {
+            return
+          }
+          return cloneElement(child, { value: index } as any)
+        })}
+      </AccordionProvider>
+    </div>
+  )
+}) as ForwardRefComponent<'div', AccordionProps>
+
+/*
+///////////////////////////////////////////////////////////////////////////////////////////////////
   Accordion Item Section
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 */
@@ -32,8 +75,8 @@ export const AccordionItem = forwardRef(({ children, value, className, ...rest }
   const id = useId()
   const itemContextValue = { id, value }
   const { corner, variant } = useAccordionContext() ?? {}
-  const classNames = classnames(`accordion--item u_${variant} `, {
-    [`u_corner-${corner}`]: !!corner,
+  const classNames = classnames(`a-c_itm u_${variant} `, {
+    [`u_rad-${corner}`]: !!corner,
     [className!]: !!className,
   })
 
@@ -56,7 +99,7 @@ export const AccordionButton = forwardRef((props, ref) => {
   const { value, id: idContext } = useAccordionItemContext() ?? {}
   const { toggleAccordion, isItemOpen } = useAccordionContext() ?? {}
   const { children, id = idContext, icon = <ChevronDown width="1em" />, className, ...rest } = props
-  const classNames = classnames('accordion--button u_between', { [className!]: !!className })
+  const classNames = classnames('a-c_btn u_between', { [className!]: !!className })
   return (
     <button
       type="button"
@@ -86,7 +129,7 @@ export const AccordionPanel = forwardRef((props, ref) => {
   const { value } = useAccordionItemContext() ?? {}
   const { isItemOpen, id } = useAccordionContext() ?? {}
 
-  const _className = classnames('accordion--panel', { [className!]: !!className })
+  const _className = classnames('a-c_pnl', { [className!]: !!className })
 
   return (
     <div id={id} data-open={isItemOpen?.(value!)} className={_className} ref={ref} {...rest}>
@@ -96,49 +139,6 @@ export const AccordionPanel = forwardRef((props, ref) => {
 }) as ForwardRefComponent<'div', AccordionPanelProps>
 
 AccordionPanel.displayName = 'Pillar-AccordionPanel'
-
-/*
-///////////////////////////////////////////////////////////////////////////////////////////////////
-  Accordion  Section
-///////////////////////////////////////////////////////////////////////////////////////////////////
-*/
-
-export const Accordion = forwardRef((props, forwardedRef) => {
-  const {
-    children,
-    type = 'single',
-    collapsible,
-    color,
-    variant,
-    size = 'md',
-    corner = 'sharp',
-    separate,
-    className,
-    ...rest
-  } = props
-  const context = useAccordion(props)
-
-  const classNames = classnames(`accordion l_flow__3xs`, {
-    [`u_${color}`]: !!color,
-    [`u_size-${size}`]: !!size,
-    l_flow: !!separate,
-    [className!]: !!className,
-  })
-  const accordionContext = { ...context, variant, corner }
-
-  return (
-    <div ref={forwardedRef} className={classNames} {...rest}>
-      <AccordionProvider {...accordionContext}>
-        {Children.map(children, (child, index) => {
-          if (!isValidElement(child)) {
-            return
-          }
-          return cloneElement(child, { value: index } as any)
-        })}
-      </AccordionProvider>
-    </div>
-  )
-}) as ForwardRefComponent<'div', AccordionProps>
 
 Accordion.displayName = 'Accordion'
 
