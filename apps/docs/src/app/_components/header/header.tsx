84ae89ecb698a88ceadda35ddffe1a20ca63a1de
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2024-11-30 14:18:15 +0100

    fix: remove the menu from the pages that not inside docs

--- apps/docs/src/app/_components/header/header.tsx
+++ apps/docs/src/app/_components/header/header.tsx
@@ -5,13 +5,16 @@ import { default as Logo } from '../logo'
 import { Flex, IconButton, InputSearch, Paper } from '@pillar-ui/core'
 import { Menu, ListSearch, Close } from '@pillar-ui/icons'
 import Link from 'next/link'
-import { useBooleanState, useDirection } from '~/hooks/pillar'
+import { useBooleanState } from '~/hooks/pillar'
 import { SwitchMode } from './switchMode'
 import { useDrawer } from '../drawerProvider'
+import { usePathname } from 'next/navigation'
 
 const Header = () => {
   const { value, setTrue, setFalse } = useBooleanState(false)
   const { toggleDrawer } = useDrawer() ?? {}
+  const pathname = usePathname()
+  const hasDocs = pathname.startsWith('/docs')
 
   return (
     <Flex className="header" as="header" justify="between" items="center" gap="4">
@@ -19,7 +22,16 @@ const Header = () => {
         <Logo width="80px" className="header--logo" />
       </Link>
 
-      <Flex as={Paper} corner="4" data-show={value} justify="between" gap="4" className="header--input-search F-c fl-2">
+      <Flex
+        as={Paper}
+        pl="4"
+        pr="4"
+        corner="4"
+        data-show={value}
+        justify="between"
+        gap="4"
+        className="header--input-search F-c fl-2"
+      >
         <div className="fl-1">
           <InputSearch fluid name="search" placeholder="Search" aria-label="search" />
         </div>
@@ -44,14 +56,16 @@ const Header = () => {
           title="Show Search"
         />
 
-        <IconButton
-          size="4"
-          variant="soft"
-          onClick={toggleDrawer}
-          className="only-mobile"
-          icon={<Menu />}
-          title="Turn on light mode"
-        />
+        {hasDocs && (
+          <IconButton
+            size="4"
+            variant="soft"
+            onClick={toggleDrawer}
+            className="only-mobile"
+            icon={<Menu />}
+            title="Turn on light mode"
+          />
+        )}
       </Flex>
     </Flex>
   )
