92687fb89c53e42661316f690110c872bf4ef717
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2025-02-17 18:25:05 +0100

    fix: skeleton avatar variant issue

--- apps/docs/src/app/_components/playground/skeleton/skeletonAvatarCorner.tsx
+++ apps/docs/src/app/_components/playground/skeleton/skeletonAvatarCorner.tsx
@@ -12,25 +12,25 @@ export const SkeletonAvatarCorner = () => {
   return (
     <Flex items="center" gap="5">
       <SkeletonAvatar corner="1" isLoading>
-        <Avatar size="1" variant="image" title="Hello" />
+        <Avatar size="1" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar corner="2" isLoading>
-        <Avatar size="2" variant="image" title="Hello" />
+        <Avatar size="2" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar corner="3" isLoading>
-        <Avatar size="3" variant="image" title="Hello" />
+        <Avatar size="3" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar corner="4" isLoading>
-        <Avatar size="4" variant="image" title="Hello" />
+        <Avatar size="4" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar corner="5" isLoading>
-        <Avatar size="5" variant="image" title="Hello" />
+        <Avatar size="5" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar corner="circle" isLoading>
-        <Avatar size="7" variant="image" title="Hello" />
+        <Avatar size="7" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar corner="full" isLoading>
-        <Avatar size="8" variant="image" title="Hello" />
+        <Avatar size="8" title="Hello" />
       </SkeletonAvatar>
     </Flex>
   )
--- apps/docs/src/app/_components/playground/skeleton/skeletonAvatarSize.tsx
+++ apps/docs/src/app/_components/playground/skeleton/skeletonAvatarSize.tsx
@@ -1,34 +1,33 @@
 'use client'
 
 import { Avatar, Flex, SkeletonAvatar } from '@pillar-ui/core'
-import { Meta } from '@storybook/react'
 
 export const SkeletonAvatarSize = () => {
   return (
     <Flex items="center" gap="5">
       <SkeletonAvatar size="1" isLoading={true}>
-        <Avatar size="1" variant="image" title="Hello" />
+        <Avatar size="1" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar size="2" isLoading={true}>
-        <Avatar size="2" variant="image" title="Hello" />
+        <Avatar size="2" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar size="3" isLoading={true}>
-        <Avatar size="3" variant="image" title="Hello" />
+        <Avatar size="3" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar size="4" isLoading={true}>
-        <Avatar size="4" variant="image" title="Hello" />
+        <Avatar size="4" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar size="5" isLoading={true}>
-        <Avatar size="5" variant="image" title="Hello" />
+        <Avatar size="5" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar size="7" isLoading={true}>
-        <Avatar size="7" variant="image" title="Hello" />
+        <Avatar size="7" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar size="8" isLoading={true}>
-        <Avatar size="8" variant="image" title="Hello" />
+        <Avatar size="8" title="Hello" />
       </SkeletonAvatar>
       <SkeletonAvatar size="9" isLoading={true}>
-        <Avatar size="9" variant="image" title="Hello" />
+        <Avatar size="9" title="Hello" />
       </SkeletonAvatar>
     </Flex>
   )
