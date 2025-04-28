fd94ba6d4d5ee80b0c239a0478c61c911c3a6ebd
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2025-04-28 16:28:02 +0100

    fix: remove examples from jsDocs and fix typescript issue in getURLParameters

--- packages/pillar-utils/src/composeEventHandlers/index.ts
+++ packages/pillar-utils/src/composeEventHandlers/index.ts
@@ -5,15 +5,6 @@
  * @param {...((event: T) => void)} handlers - The event handlers to compose.
  * @returns {(event: T) => void} - The composed event handler.
  */
-// export function composeEventHandlers<T extends MouseEvent>(...handlers: ((event: T) => void)[]): (event: T) => void {
-//   return (event: T) => {
-//     for (const handler of handlers) {
-//       if (typeof handler === 'function') {
-//         handler(event)
-//       }
-//     }
-//   }
-// }
 
 export const composeEventHandlers =
   (...handlers: ((event: Event) => void)[]) =>
--- packages/pillar-utils/src/isValidDate/index.ts
+++ packages/pillar-utils/src/isValidDate/index.ts
@@ -1,10 +1,4 @@
 /**
- * Checks if a year is a leap year.
- * @param {number} year - The year to check.
- * @returns {boolean} `true` if the year is a leap year, `false` otherwise.
- */
-
-/**
  * Checks if a string is a valid date.
  * @param {string} dateString - The string to check.
  * @returns {boolean} - Returns true if the string is a valid date, false otherwise.
